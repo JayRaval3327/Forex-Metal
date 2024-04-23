c76a484a896f0734e4be7ca119a56e5478409042
Jay <jraval93@gmail.com>
2024-04-22 20:39:10 -0400

    Initial Commit

---
+++ Forex-Metal/Forex_MetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forex_MetalApp.swift
+//  Forex-Metal
+//
+//  Created by Jay Raval on 2024-04-22.
+//
+
+import SwiftUI
+
+@main
+struct Forex_MetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
